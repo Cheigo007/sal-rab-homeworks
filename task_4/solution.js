30ad3877c42dfb998a6f7728a093f4701436c4e7
Igc <cheigo@yandex.ru>
2022-12-26 03:00:29 +0500

    M-4.3

--- task_4/solution.js
+++ task_4/solution.js
@@ -8,7 +8,7 @@ function parseProducts(json) {
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
     let data;
-    data.products = JSON.parse(json);
+    data = JSON.parse(json);
     let products;
     products = data.products;
     return ;
@@ -26,7 +26,8 @@ function renderProductsCards(json){     // дальнейшие действия
     clearProducts ();
     let products;
     products = parseProducts(json);
-    let length = products.length;
+    let length; 
+    length = products.length;
     for (let i = 0; i < length; i += 1) {
         addProduct(products[i]); 
      }
