48dda08eeb9516fd897c13aae18e57d0bf8db760
Igc <cheigo@yandex.ru>
2023-01-09 22:10:30 +0500

    Modified 5.0

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    data = {
+        client: { }, 
+        order:  {sum, address}, 
+        goods:  [ ]
+    };
+    
+    countOfGoods = goods.length;
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (i = 0; i < countOfGoods; i +=1) {
+        data.goods.push(
+            {title:goods[i].title, 
+            count:goods[i].count}
+            );
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
-}
+    data.client = name + " " + phone;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", "+ address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.sum = sum;
+    
+    jsonData = JSON.stringify({data: data});
+    
+    return (jsonData)
+    }
